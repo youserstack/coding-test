693be9dfd9f4ec27963e6dbed90d79f61554e663
youserstack <163319030+youserstack@users.noreply.github.com>
2025-09-30 12:40:14 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -2,6 +2,8 @@ function solution(n, k) {
     // 양꼬치n인분, 음료수k개
     // 양꼬치1인분 = 12000, 음료수1개 = 2000
     // 10인분 -> 음료수서비스 서비스
-    const service = (Math.floor(n/10))*2000
-    return n*12000+k*2000-service
+    const yanggochi = 12000
+    const drink = 2000
+    const service = (Math.floor(n/10))*drink
+    return n*yanggochi+k*drink-service
 }
